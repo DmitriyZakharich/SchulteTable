e902f0e693aa6069e2aaf8ba04fd9cabb625add9
Dima <deezee47@yandex.ru>
2021-07-31 21:28:56 +0300

    Completed class StatisticAdapter

--- app/src/main/java/ru/schultetabledima/schultetable/ActivityStatistics.java
+++ app/src/main/java/ru/schultetabledima/schultetable/ActivityStatistics.java
@@ -2,24 +2,19 @@ package ru.schultetabledima.schultetable;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.CursorLoader;
-import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class ActivityStatistics extends AppCompatActivity {
 
     DatabaseController dbController;
     Cursor cursor;
-    StatisticsAdapter statisticsAdapter;
-
+    StatisticAdapter statisticAdapter;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -31,13 +26,11 @@ public class ActivityStatistics extends AppCompatActivity {
         TextView textView = (TextView)findViewById(R.id.textView);
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
 
-
         dbController = new DatabaseController(this);
         cursor =  dbController.getCursor();
 
-
-        statisticsAdapter = new StatisticsAdapter(this, cursor);
-        recyclerView.setAdapter(statisticsAdapter);
+        statisticAdapter = new StatisticAdapter(cursor, this);
+        recyclerView.setAdapter(statisticAdapter);
     }
 
 
