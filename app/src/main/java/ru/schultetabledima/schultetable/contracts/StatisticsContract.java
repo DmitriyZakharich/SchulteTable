5af8fe71c0e74056af09a791e23adbc7dbc77539
Dima <zakharichd@yandex.ru>
2021-09-16 16:47:17 +0300

    Added SharedPreferences to StatisticsPresenter

--- app/src/main/java/ru/schultetabledima/schultetable/contracts/StatisticsContract.java
+++ app/src/main/java/ru/schultetabledima/schultetable/contracts/StatisticsContract.java
@@ -3,13 +3,13 @@ package ru.schultetabledima.schultetable.contracts;
 import android.widget.ArrayAdapter;
 
 import moxy.MvpView;
-import moxy.viewstate.strategy.AddToEndStrategy;
+import moxy.viewstate.strategy.AddToEndSingleStrategy;
 import moxy.viewstate.strategy.StateStrategyType;
 import ru.schultetabledima.schultetable.statistic.database.StatisticAdapter;
 
 public interface StatisticsContract {
 
-    @StateStrategyType(AddToEndStrategy.class)
+    @StateStrategyType(AddToEndSingleStrategy.class)
     interface View extends MvpView{
 
         void setRecyclerViewAdapter(StatisticAdapter statisticAdapter);
@@ -19,6 +19,10 @@ public interface StatisticsContract {
         void setValueTypeAdapter(ArrayAdapter<String> adapterValueType);
 
         void setPlayedSizesAdapter(ArrayAdapter<String> adapterPlayedSizes);
+
+        void setSelectionQuantityTables(int position);
+        void setSelectionSpinnerValueType(int position);
+        void setSelectionPlayedSizes(int position);
     }
 
     interface Presenter {
