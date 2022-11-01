48c54be3b44f52dc0d7988b592a2a563ae87ae85
Dima <zakharichd@yandex.ru>
2022-11-01 17:21:11 +0300

    Refactoring DeletePopupMenuCreator.

--- app/src/main/java/ru/schultetabledima/schultetable/statistic/DeletePopupMenuCreator.kt
+++ app/src/main/java/ru/schultetabledima/schultetable/statistic/DeletePopupMenuCreator.kt
@@ -11,30 +11,24 @@ import ru.schultetabledima.schultetable.database.Result
  * Class for deleting records from DB
  * */
 class DeletePopupMenuCreator(
-    val presenter: StatisticsPresenter,
-    val context: Context?,
-    val view: View?,
+    private val presenter: StatisticsPresenter,
+    context: Context?,
+    view: View?,
     private val result: Result,
-    val position: Int
+    private val position: Int
 ) {
-    private lateinit var popupMenu: PopupMenu
+    private val popupMenu: PopupMenu
 
     init {
-        main()
-    }
-
-    private fun main() {
         val wrapper: Context = ContextThemeWrapper(context, R.style.PopupMenuStyle)
         popupMenu = PopupMenu(wrapper, view!!)
         popupMenu.inflate(R.menu.statistic_menu)
 
         popupMenu.setOnMenuItemClickListener { item: MenuItem? ->
 
-            when (item!!.itemId) {
-                R.id.item_delete -> {
-                    presenter.deleteRecordFromDB(result, position)
-                }
-            }
+            if  (item!!.itemId == R.id.item_delete)
+                presenter.deleteRecordFromDB(result, position)
+
             true
         }
     }
